898fcb758e47d0c29e84be121537bef97c52ebcb
berktsyrk <116994507+berktsyrk@users.noreply.github.com>
2024-07-31 16:33:29 +0300

    Add files via upload

---
+++ ayarlar.js
@@ -0,0 +1,4 @@
+module.exports = {
+    token: 'MTI1NDAyOTAyOTA4Njk4NjI3MQ.GUI-26.rNffSGyjNsDpKq_TJjk2g48DVTSFQLcoH4q1L0', 
+    sahipID: '1205808474223808562',
+  };
---
+++ modules/sorular.js
@@ -0,0 +1,7 @@
+module.exports = {
+    soru1: 'Yaşın kaç?',
+    soru2: 'Daha önce hiç yetkili oldun mu?',
+    soru3: 'Admin komutlarını biliyormusun?',
+    soru4: 'Kaç saat sunucuda atkif olabilirsin',
+    soru5: 'Adın ne?',
+  };
